fbfc5ed8597329b03731680cc6d86d68ba2d85e4
ipmanlk <io@navinda.xyz>
2019-06-01 11:54:28 +0530

    change: permission handling improved

--- controllers/permission.js
+++ controllers/permission.js
@@ -1,12 +1,19 @@
-const check = (msg, allowed=[], disallowed=[]) => {
+const check = (msg, allowed = [], disallowed = []) => {
     // get all roles
     let memberRoles = msg.member.roles;
 
     // check member has at least one allowed role
-    let roleAllowed = allowed.some(role => memberRoles.some(r => r.name === role)); 
+    let roleAllowed = true;
+    if (allowed.length !== 0) {
+        roleAllowed = allowed.some(role => memberRoles.some(r => r.name === role));
+    }
+
     // check member has at least one disallowed role
-    let roleDisallowed = disallowed.some(role => memberRoles.some(r => r.name === role));
-    
+    let roleDisallowed = false;
+    if (disallowed !== 0) {
+        roleDisallowed = disallowed.some(role => memberRoles.some(r => r.name === role));
+    }
+
     // return if member has perms
     return (roleAllowed && !roleDisallowed);
 };
